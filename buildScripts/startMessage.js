14266b5afb6d65b51cb863dfb0156ea4f77955d5
Ajinkya Bhagwat <ajinkya.bhagwat@gmail.com>
2017-05-08 16:24:25 +0530

    added babel, eslint

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,3 @@
-var chalk = require('chalk');
+import chalk from 'chalk';
 
-console.log(chalk.green('Starting app in dev env ... '));
+console.log(chalk.green('Starting app in dev env ... '));//eslint-disable-line no-console
